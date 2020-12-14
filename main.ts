fb2323f9b57aa9e6befb5a2799b7c749a50385d7
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-14 10:53:44 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ let Température = 0
 basic.forever(function () {
     Température = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P1)
     basic.showNumber(Température)
+    basic.pause(5000)
 })
